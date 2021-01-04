266bdbf9d6fc68f5401b8a9115925352a1f5ab26
ningpeng <2254824749@qq.com>
2021-01-04 18:06:59 +0800

    v2021.0104.0001

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
-    let a = 12;
-    println!("a :" ,a);
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main(){
+    //let text = fs::read_to_string("D:\\123.txt").unwrap();
+    //let text = fs::read("D:\\123.txt").unwrap();
+    let mut file = File::create("D:\\123.txt").unwrap();
+    file.write(b"hjsrgfjskdhf").unwrap();
+    //println!("{:?}",text);
 }
